4c9b63265cd99fa60e763c31fcf81e7898f50a97
bartek <boratek.3@gmail.com>
2017-11-12 20:26:38 +0100

    add insert example

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,5 +6,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
+  db.collection('Todos').insertOne(
+    {
+      text: 'Sth to do',
+      completed: false
+    }, (err, result) => {
+      if (err) {
+        return console.log('Unable to insert todo', err);
+      }
+
+      console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
   db.close();
 });
