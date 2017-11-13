5a58a5a4847a0cbb0b8d22cbe59325ddecc8088c
bartek <boratek.3@gmail.com>
2017-11-13 20:15:06 +0100

    ch-65 select count

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -23,15 +23,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // });
 
   // fetch * where _id = '5a08a05e643f7a1ff243a6af'
-    db.collection('Todos').find({
-      _id: new ObjectID('5a08a05e643f7a1ff243a6af') // the only way to load document by id
-    }).toArray().then((docs) => {
-      console.log('Todos');
-      console.log(JSON.stringify(docs, undefined, 2));
-    }, (err) => {
-      console.log('Unable to fetch todos', err);
-    });
+    // db.collection('Todos').find({
+    //   _id: new ObjectID('5a08a05e643f7a1ff243a6af') // the only way to load document by id
+    // }).toArray().then((docs) => {
+    //   console.log('Todos');
+    //   console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //   console.log('Unable to fetch todos', err);
+    // });
 
+// fetch count(*)
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
 
   // db.close();
 });
