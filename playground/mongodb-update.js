1b2dad85a0904320148419e38711b1efc4003672
bartek <boratek.3@gmail.com>
2017-11-16 21:38:51 +0100

    ch-68 - add update examples

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -6,5 +6,38 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
+// simple update
+  // db.collection('Todos').findOneAndUpdate(
+  //   {
+  //     _id: new ObjectID('5a0b5a4ee72aa1dc78436e50')
+  //   }, {
+  //     $set: {
+  //       completed: true
+  //     }
+  //   }, {
+  //     returnOriginal: false
+  //   }
+  //   ).then((result) => {
+  //     console.log(result);
+  //   });
+
+// update with incrementation
+  db.collection('Users').findOneAndUpdate(
+    {
+      _id: new ObjectID('5a0debd1c8f340c717706dd8')
+    }, {
+      $set: {
+        name: 'Zbigniew',
+      },
+      $inc: {
+        age: 1
+      }
+    }, {
+      returnOriginal: false
+    }
+    ).then((result) => {
+      console.log(result);
+    });
+
   // db.close();
 });
