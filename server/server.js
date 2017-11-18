e26bcb3faafe3dab251a7cfcb756f86129d243a3
bartek <boratek.3@gmail.com>
2017-11-18 19:42:04 +0100

    ch-69 - add server config and prepare model

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//defining todo model
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
