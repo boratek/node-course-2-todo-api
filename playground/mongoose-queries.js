faf7a471df873dbe3bf758d39e4c3d020e2f2a4b
bartek <boratek.3@gmail.com>
2017-11-25 15:06:25 +0100

    ch-77 - load use by user id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,23 +1,44 @@
+const {ObjectId} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
 var id = '5a19391f23b9be950d639317';
+var userId = '5a11e95f83ad3c323aa1dcf1';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
 
 // returns [] with object loaded by id
-Todo.find({
-  _id: id // mongoose will convert string to ObjectID himself
-}).then((todos) => {
-  console.log('Todos by find()', todos);
-});
+// Todo.find({
+//   _id: id // mongoose will convert string to ObjectID himself
+// }).then((todos) => {
+//   console.log('Todos by find()', todos);
+// });
+//
+// // returns one {}
+// Todo.findOne({
+//   _id: id // mongoose will convert string to ObjectID himself
+// }).then((todo) => {
+//   console.log('Todo by findOne()', todo);
+// });
 
 // returns one {}
-Todo.findOne({
-  _id: id // mongoose will convert string to ObjectID himself
-}).then((todo) => {
-  console.log('Todo by findOne()', todo);
-});
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//
+//   console.log('Todo by findById()', todo);
+// }).catch((e) => console.log(e));
 
-// returns one {}
-Todo.findById(id).then((todo) => {
-  console.log('Todo by findById()', todo);
-});
+// returns one user {}
+User.findById(userId).then((user) => {
+  if (!user) {
+    return console.log('user id not found');
+  }
+
+  console.log('User by findById()', user);
+}).catch((e) => console.log(e));
