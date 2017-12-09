3c9ebf54d82b8fa1fa0074141f30d231ba6a94d9
bartek <boratek.3@gmail.com>
2017-12-09 21:56:17 +0100

    add DELETE/ route to server

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -15,7 +15,6 @@ Todo.findByIdAndRemove({'_id': '5a2c48cec8cedb415b037a31'}).then((todo) => {
   console.log(todo);
 });
 
-
 // Todo.findByIdAndRemove() - remove and return what has been removed
 Todo.findByIdAndRemove('5a2c48cec8cedb415b037a31').then((todo) => {
   console.log(todo);
