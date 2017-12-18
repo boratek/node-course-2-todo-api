c05fd059f7e2bcb8a0313e9a5716cc3119fce3ff
bartek <boratek.3@gmail.com>
2017-12-18 18:35:00 +0100

    ch-86 - move connection config to separate file and depends it on env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://todo:todo@ds129156.mlab.com:29156/todo');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
