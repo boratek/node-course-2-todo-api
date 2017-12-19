5cd3f395f46e0ef2dc89607876608d1317c3cd68
bartek <boratek.3@gmail.com>
2017-12-19 21:34:32 +0100

    ch-88 - prepare email validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,14 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// {
+//   email: 'some@email.com',
+//   password: '3q4tsergsdfgdf',
+//   tokens: [{
+//     access: 'auth',
+//     token: 'gq34grsergdfx'
+//   }]
+// }
 
 // define user model
 var User = mongoose.model('User', {
@@ -6,7 +16,14 @@ var User = mongoose.model('User', {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
+    trim: true,
+    unique: true,
+    validate: {
+      validator: (value) => {
+        return validator.isEmail(value);
+      },
+      message: '{VALUE} is not a valid email'
+    }
   }
 });
 
