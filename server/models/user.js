cfe0393ee19ba3acc60ed9832a533b1523e395cd
bartek <boratek.3@gmail.com>
2017-12-19 21:37:29 +0100

    ch-88 - add pass and token to user model

--- server/models/user.js
+++ server/models/user.js
@@ -19,12 +19,25 @@ var User = mongoose.model('User', {
     trim: true,
     unique: true,
     validate: {
-      validator: (value) => {
-        return validator.isEmail(value);
-      },
+      validator: validator.isEmail,
       message: '{VALUE} is not a valid email'
     }
-  }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
