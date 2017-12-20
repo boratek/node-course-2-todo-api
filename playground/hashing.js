77fb8cd4e6154e7b45e43e38346da0f40f803cc2
bartek <boratek.3@gmail.com>
2017-12-20 22:46:59 +0100

    ch-89 - test jsonwebtoken

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
+console.log('\n---------------- USE JSONWEBTOKEN -------------------- \n');
+
+var data = {
+  id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(`Encoded: ${token}`);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('Decoded:', decoded);
+
+// MAN IN THE MIDDLE
+console.log('\n---------------- MAN IN THE MIDDLE -------------------- \n');
 var message = 'I am user nr 3';
 var hash = SHA256(message).toString();
 
