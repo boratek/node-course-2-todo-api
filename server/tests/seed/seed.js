b257cb8a3ac2fc12b0bd96e3ed6b9fd84c7b5fa9
bartek <boratek.3@gmail.com>
2017-12-27 19:29:18 +0100

    ch-93 - add users data for tests

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,6 +1,27 @@
 const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
 const {Todo} = require('./../../models/todo');
 const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [
+  {
+    _id: userOneId,
+    email: 'bart@op.pl',
+    password: 'userOnePass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    }]
+  }, {
+    _id: userTwoId,
+    email: 'krzes@op.pl',
+    password: 'userTwoPass'
+  }
+];
 
 const todos = [
   {
@@ -23,4 +44,13 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
-module.exports = {todos, populateTodos};
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
