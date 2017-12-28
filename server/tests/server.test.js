8c55254eb895a5c74c98196f3d9f6d974555426c
bartek <boratek.3@gmail.com>
2017-12-28 14:32:26 +0100

    ch-94 - add tests to users/me routes

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -181,3 +181,28 @@ describe('PATCH /todos/:id', () => {
 
   });
 });
+
+describe('GET /users/me', () => {
+  it('should return a user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return a 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      // .set('x-auth', users[1].tokens[1].token)
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
