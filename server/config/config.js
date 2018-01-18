e297ff9c82a7e4964fed72a1f12a7ef06def4b44
bartek <boratek.3@gmail.com>
2018-01-18 19:10:00 +0100

    ch-101 - use config set secret for tests

--- server/config/config.js
+++ server/config/config.js
@@ -2,9 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 if ('development' === env || 'test' === env) {
   var config = require('./config.json');
-  console.log(config);
   var envConfig = config[env];
-  console.log(envConfig);
 
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key]
