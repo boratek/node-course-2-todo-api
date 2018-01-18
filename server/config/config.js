91f54ca967cd97a0e00f0c946f823819c884171a
bartek <boratek.3@gmail.com>
2018-01-18 18:56:34 +0100

    ch-101 - move db connect config to separate config json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,16 @@
 var env = process.env.NODE_ENV || 'development';
 
-if ('development' === env) {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if ('test' === env) {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if ('production' == env) {
+if ('development' === env || 'test' === env) {
+  var config = require('./config.json');
+  console.log(config);
+  var envConfig = config[env];
+  console.log(envConfig);
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key]
+  });
+}
+
+if ('production' === env) {
   process.env.MONGODB_URI = 'mongodb://todo:todo@ds129156.mlab.com:29156/todo';
 }
